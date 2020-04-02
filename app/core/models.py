e9ab914225e5961f42e89d3dc5b96d929d0eea5c
Benning <60070109@kmitl.ac.th>
2020-04-02 23:09:08 +0700

    add name to category

--- app/core/models.py
+++ app/core/models.py
@@ -141,6 +141,9 @@ class Categorie(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Need(models.Model):
     """Need of hospital"""
