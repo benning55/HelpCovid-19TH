ef70f27db8b9b89d9158f42b8a0060853e2d3e2b
Benning <60070109@kmitl.ac.th>
2020-04-16 20:26:33 +0700

    add pic

--- front-end-covit-help-master/src/components/Footer.vue
+++ front-end-covit-help-master/src/components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
     <footer id="sticky-footer" class="py-4 bg-light_green text-white-50">
         <div class="container text-center">
-            <small class="">Copyright &copy; Covid19th.org</small>
+            <small>Copyright &copy; Covid19th.org</small>
+            <img src="https://scontent.fbkk2-8.fna.fbcdn.net/v/t1.0-9/93691592_236420717768211_8908826615123804160_n.png?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=yMO3IONMKa0AX-38zUS&_nc_ht=scontent.fbkk2-8.fna&oh=45ee06610bce63c0b2791ba9732a0f35&oe=5EBD4CCF" alt="" style="width: 25%; height: 10%">
+            <img src="https://scontent.fbkk2-8.fna.fbcdn.net/v/t1.0-9/93789891_236420671101549_2080523149350273024_n.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=gtmaOlz3qtoAX8X_uG1&_nc_ht=scontent.fbkk2-8.fna&oh=f7dc7e96b0daa92515254c4cd6e28a28&oe=5EBEC06C" alt="">
         </div>
     </footer>
 </template>
